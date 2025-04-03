c385efa163b080a4b34176934f3798e13caa6862
Raina Shahbuddin <rains@seas.upenn.edu>
2025-04-03 14:37:47 +0000

    fixed send_message function

--- submitProof.py
+++ submitProof.py
@@ -90,8 +90,8 @@ def send_signed_msg(proof, random_leaf):
     })
 
     signed_tx = w3.eth.account.sign_transaction(tx, acct.key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    return tx_hash.hex()
+    return w3.eth.send_raw_transaction(signed_tx.signedTransaction)
+
 
 
 # Helper functions that do not need to be modified
