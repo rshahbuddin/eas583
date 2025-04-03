71f67aa908d80cd05978d1ad9cf4b6e7487b4201
Raina Shahbuddin <rains@seas.upenn.edu>
2025-04-03 18:18:02 +0000

    trying to fix last two tests

--- submitProof.py
+++ submitProof.py
@@ -100,7 +100,7 @@ def send_signed_msg(proof, random_leaf):
     signed_tx = w3.eth.account.sign_transaction(tx, acct.key)
 
     try:
-        tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_hash = w3.eth.send_raw_transaction(signed_tx['rawTransaction'])
         print(f"Transaction sent: {tx_hash.hex()}")
         return tx_hash.hex()
     except Exception as e:
